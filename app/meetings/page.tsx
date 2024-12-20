17206d8d78bde7a23da7ffd8de3eb0e0d364a5f0
Ashaheen92 <ahmedsayed.1592@gmail.com>
2024-12-20 11:31:19 +0200

    feat: add Radix UI Select component and enhance meeting page layout; improve calendar time display

--- app/meetings/page.tsx
+++ app/meetings/page.tsx
@@ -4,6 +4,15 @@ import { Calendar } from '@/components/ui/calendar';
 import { Card, Separator, Button } from '@/components/ui';
 import { Fragment, useEffect, useRef, useState } from 'react';
 import { DateTime } from 'luxon';
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
 
 const Background = dynamic(() => import('@/components/background'), {
   ssr: false,
@@ -26,18 +35,20 @@ const generateTimeSlots = (
   return times;
 };
 
-const getTimeString = () =>
-  DateTime.local().toFormat("EEEE, MMM dd, yyyy 'at' hh:mm:ss a '['z']'");
-
 export default function Meeting() {
   const timeSlots = generateTimeSlots('7:45 pm', 15, 15);
 
-  const timeRef = useRef<HTMLSpanElement | null>(null);
+  const timeLocalRef = useRef<HTMLSpanElement | null>(null);
+  const timeUSARef = useRef<HTMLSpanElement | null>(null);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
-      if (timeRef.current) {
-        timeRef.current.textContent = getTimeString();
+      if (timeLocalRef.current && timeUSARef.current) {
+        timeLocalRef.current.textContent =
+          DateTime.local().toFormat('z: hh:mm:ss a');
+        timeUSARef.current.textContent = DateTime.local({
+          zone: 'America/New_York',
+        }).toFormat('z: hh:mm:ss a');
       }
     }, 1000);
 
@@ -51,21 +62,79 @@ export default function Meeting() {
   return (
     <Background>
       <div className='flex h-full items-center justify-center'>
-        <Card className='h-fit w-fit'>
+        <Card className='w-[340px] md:w-[768px]'>
           <Card.Header>
-            <Card.Title>Curenta</Card.Title>
+            <Card.Title>Meeting duration</Card.Title>
             <Card.Description>
               Find a time to meet with Curenta.
             </Card.Description>
           </Card.Header>
           <Card.Content>
-            <div className='flex h-[300px] w-full justify-center gap-3'>
-              <Calendar mode='single' selected={date} onSelect={setDate} />
-              <Separator orientation='vertical' />
+            <div className='flex h-fit justify-center gap-3 md:h-[470px]'>
+              <div className='relative flex flex-col justify-items-stretch gap-3'>
+                <Calendar
+                  mode='single'
+                  selected={date}
+                  onSelect={setDate}
+                  disabled={{ before: new Date(), after: new Date(2025, 0, 4) }}
+                />
 
-              <div className='flex flex-col items-start'>
+                <p className='absolute bottom-0 hidden text-sm md:block'>
+                  Time Now:
+                  <br />
+                  <span ref={timeLocalRef}>
+                    {DateTime.local().toFormat('z: hh:mm:ss a')}
+                  </span>
+                  <br />
+                  <span ref={timeUSARef}>
+                    {DateTime.local({
+                      zone: 'America/New_York',
+                    }).toFormat('z: hh:mm:ss a')}
+                  </span>
+                </p>
+
+                <Separator className='block md:hidden' />
+
+                <div className='flex w-full flex-col gap-2 md:hidden'>
+                  <h1 className='font-medium'>What time works best?</h1>
+
+                  <Select>
+                    <SelectTrigger className='w-full'>
+                      <SelectValue
+                        placeholder={
+                          <p className='text-xs'>
+                            Showing times for&nbsp;
+                            <strong className='font-medium'>
+                              {date
+                                ? date.toLocaleString('en-us', {
+                                    dateStyle: 'long',
+                                  })
+                                : new Date()?.toLocaleString('en-us', {
+                                    dateStyle: 'long',
+                                  })}
+                            </strong>
+                          </p>
+                        }
+                      />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectGroup>
+                        {timeSlots.map((slot) => (
+                          <SelectItem key={slot} value={slot}>
+                            {slot}
+                          </SelectItem>
+                        ))}
+                      </SelectGroup>
+                    </SelectContent>
+                  </Select>
+                </div>
+              </div>
+
+              <Separator orientation='vertical' className='hidden md:block' />
+
+              <div className='hidden w-96 flex-col items-start md:flex'>
                 <h1 className='font-medium'>What time works best?</h1>
-                <caption>
+                <p>
                   Showing times for&nbsp;
                   <strong className='font-medium'>
                     {date
@@ -74,12 +143,9 @@ export default function Meeting() {
                           dateStyle: 'long',
                         })}
                   </strong>
-                </caption>
-                <p>
-                  Time Now:
-                  <span ref={timeRef}>{getTimeString()}</span>
                 </p>
-                <div className='flex h-96 w-full flex-col gap-2 overflow-y-scroll'>
+
+                <div className='mt-2 flex h-[470px] w-full flex-col gap-2 overflow-y-scroll'>
                   {timeSlots.map((slot) => (
                     <Button key={slot} variant='outline'>
                       {slot}
