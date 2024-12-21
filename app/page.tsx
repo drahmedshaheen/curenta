237a257d9f4bcc51a4c4b2aaa716b45c45945cb7
Ashaheen92 <ahmedsayed.1592@gmail.com>
2024-12-21 10:43:36 +0200

    fix: remove unnecessary padding in home component for improved layout

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
     <Background>
       <div className='grid h-full w-full grid-cols-2 place-items-center gap-y-7 px-4 py-10 lg:px-10'>
         <div className='col-span-2 space-y-5 lg:col-span-1 lg:space-y-0'>
-          <div className='space-y-5 px-7 backdrop-blur-[2px] lg:p-12'>
+          <div className='space-y-5 backdrop-blur-[2px] lg:p-12'>
             <h1 className='w-full text-center text-3xl font-medium leading-tight text-white selection:bg-white selection:text-sky-500 sm:text-4xl md:text-5xl 2xl:text-6xl'>
               AI Empowering Senior Living Communities at Their Core
             </h1>
